85a567036715a2a0b1e61d95c8a16203adbd36c0
maheshkumarrouth <mahee.routh@gmail.com>
2015-12-23 14:44:53 +0530

    update

--- src/main/java/de/employeeApp/dao/interfaces/employeeManagementSystem/ConformationMailQueueBasicDAO.java
+++
@@ -1,9 +0,0 @@
-package de.employeeApp.dao.interfaces.employeeManagementSystem;
-
-import de.employeeApp.dao.model.employeeManagementSystem.ConformationMailQueue;
-
-public interface ConformationMailQueueBasicDAO {
-	
-	public void save(ConformationMailQueue conformationMailQueue);
-	public ConformationMailQueue findByEmployeeID();
-}
--- src/main/java/de/employeeApp/dao/interfaces/employeeManagementSystem/VerificationTokenBasicDAO.java
+++
@@ -1,7 +0,0 @@
-package de.employeeApp.dao.interfaces.employeeManagementSystem;
-
-import de.employeeApp.dao.model.employeeManagementSystem.VerificationToken;
-
-public interface VerificationTokenBasicDAO {
-	public void save(VerificationToken verificationToken);
-}
