e5a90f3ecd08357d9ad111bffea0132be854184d
maheshkumarrouth <mahee.routh@gmail.com>
2015-12-28 11:57:43 +0530

    Handle exceptions using @ControllerAdvice

--- src/main/java/de/employeeapp/controller/GlobalControllerAdvice.java
+++ src/main/java/de/employeeapp/controller/GlobalControllerAdvice.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import de.employeeapp.beans.ErrorDetail;
 import de.employeeapp.exceptions.EmailAlreadyExistedException;
 import de.employeeapp.exceptions.UserNameAlreadyExisted;
 
-@ControllerAdvice
-public class GlobalControllerAdvice {
+@ControllerAdvice(basePackages = {"de.employeeapp.controller"})
+public class GlobalControllerAdvice{
 	
 	@ExceptionHandler(EmailAlreadyExistedException.class)
-	@ResponseStatus(value=HttpStatus.NOT_FOUND)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
 	public ErrorDetail emailAlreadyExistedException(Exception exception) {
+		System.out.println("emailAlreadyExistedException");
 		ErrorDetail error = new ErrorDetail();
 		error.setStatus(HttpStatus.BAD_REQUEST.value());
 		error.setMessage(exception.getLocalizedMessage());
@@ -24,7 +28,8 @@ public class GlobalControllerAdvice {
 	}
 	
 	@ExceptionHandler(UserNameAlreadyExisted.class)
-	@ResponseStatus(value=HttpStatus.NOT_FOUND)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
 	public ErrorDetail userNameAlreadyExisted(Exception exception) {
 		ErrorDetail error = new ErrorDetail();
 		error.setStatus(HttpStatus.BAD_REQUEST.value());
