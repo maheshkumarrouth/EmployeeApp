76b159cc6b358e20f621ceaaa2f536ec297c6207
chinni-rao <chinni.krao23@gmail.com>
2016-06-26 14:11:23 +0530

    sample test case for emp service

--- src/test/java/de/employeeApp/junitClasses/EmployeeJunitClass.java
+++ src/test/java/de/employeeApp/junitClasses/EmployeeJunitClass.java
@@ -1,11 +1,44 @@
 package de.employeeApp.junitClasses;
 
-import org.junit.Test;
+import java.util.Date;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.employeeapp.beans.Employee;
+import de.employeeapp.service.EmployeeManagementServiceImpl;
+import de.employeeapp.service.ValidationService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class EmployeeJunitClass {
-	
+
+	@Autowired
+	ValidationService validationService;
+
+//	@Before
+//	public void setup() {
+//
+//		Employee employee = new Employee();
+//		employee.setEmailId("100");
+//		employee.setFirstName("Test");
+//		employee.setDateOfBirth(new Date());
+//
+//		validationService.validateEmployee(employee);
+//
+//	}
+
 	@Test
-	public void EmployeeCreationTestCase(de.employeeapp.beans.Employee employee){
-		
+	public void employeeCreationTestCase() {
+
+		EmployeeManagementServiceImpl serviceImpl = new EmployeeManagementServiceImpl();
+		Employee employee = new Employee();
+		employee.setEmailId("100");
+		employee.setFirstName("Test");
+		employee.setDateOfBirth(new Date());
+		serviceImpl.doEmployeeRegistration(employee);
 	}
 }
